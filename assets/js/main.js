09a017ddffaa4f8c5a58986f543b584bbfaedc65
Kiembe <kiemhvph43131#fpt.edu.vn>
2023-04-05 11:15:03 +0700

    two

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,21 @@ openMenu.onclick = function() {
 Menu.onclick = function() {
     Menu.classList.remove("open")
 }
+
+window.addEventListener("scroll", reveal);
+
+    function reveal() {
+        var reveals = document.querySelectorAll('.reveral');
+
+        for(var i = 0 ; i < reveals.length ; i++){
+            var windowHeight = window.innerHeight;
+            var revealTop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 150;
+
+            if(revealTop < windowHeight - revealpoint){
+                reveals[i].classList.add('active');
+            }else{
+                reveals[i].classList.remove('active');
+            }
+        }
+    }
